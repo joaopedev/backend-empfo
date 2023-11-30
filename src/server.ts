9327078770a9277ae7e5554ff8d964143b776419
joaopedev <joaopedev@outlook.com>
2023-11-29 22:16:07 -0300

    Delete paths for logs , pay service for this.

--- src/server.ts
+++ src/server.ts
@@ -1,60 +1,20 @@
-import { logger } from "./models/logger_model";
 import express, { json, Response, NextFunction, Request } from "express";
 import createError from "http-errors";
 import helmet from "helmet";
 import cors from "cors";
 import { HTTP_ERRORS } from "./models/model";
-import fs from "fs";
-import path from "path";
-import morgan from "morgan";
 const consign = require("consign");
 require("dotenv").config();
 
 const AUTHORIZATION = process.env.AUTHORIZATION;
 
 //Tratando exceções do Node
-process.on("SIGTERM", (signal) => {
-  logger.info(`Process ${process.pid} received a SIGTERM signal`);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on("SIGINT", (signal) => {
-  logger.info(`Process ${process.pid} has been interrupted`);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on("uncaughtException", (err) => {
-  logger.error(`Uncaught Exception: ${err.message}`);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled rejection at ", promise, `reason: ${reason}`);
-  server.close(() => {
-    process.exit(1);
-  });
-});
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(json());
-
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "logs/access.log"),
-  { flags: "a" }
-);
-
-// Logs morgan for acess logs from rote
-// Setup the logger
-app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use("/private/*", (req: Request, res: Response, next: NextFunction) => {
   let authorization = req.header("authorization");
@@ -80,5 +40,5 @@ app.use((error: any, req: Request, res: Response, next: NextFunction) => {
 });
 
 const server = app.listen(Number(process.env.PORT), () => {
-  logger.info(`Servidor rodando na porta ${process.env.PORT}`);
+  console.log(`Servidor rodando na porta ${process.env.PORT}`);
 });
