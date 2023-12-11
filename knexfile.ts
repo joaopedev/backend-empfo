2bad487d7104b516ff791b305678108c14918c7f
joaopedev <joaopedev@outlook.com>
2023-12-11 20:36:39 -0300

    hot-fix

--- knexfile.ts
+++ knexfile.ts
@@ -5,6 +5,7 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: "pg",
     connection: {
+      host:process.env.PGHOST,
       user: process.env.PGUSER,
       password: process.env.PGPASSWORD,
       database: process.env.PGDATABASE,
