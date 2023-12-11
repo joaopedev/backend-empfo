3c4493ef531b335f8d3238a07f3a2c9c6c7faa1c
joaopedev <joaopedev@outlook.com>
2023-12-11 19:37:15 -0300

    Fix knex

--- knexfile.ts
+++ knexfile.ts
@@ -7,16 +7,33 @@ const config: { [key: string]: Knex.Config } = {
     connection: {
       user: process.env.PGHOST,
       password: process.env.PGPASSWORD,
-      database: process.env.PGDATABASE
+      database: process.env.PGDATABASE,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
       tableName: "knex_migrations",
-      directory: './migrations',
-    }
+      directory: "./migrations",
+    },
+  },
+  production: {
+    client: "postgresql",
+    connection: {
+      host: process.env.PGHOST,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./migrations",
+    },
   },
 };
 
