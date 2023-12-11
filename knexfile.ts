b69c4d71e2ab8205879e31c160bb83c310a75f7b
joaopedev <joaopedev@outlook.com>
2023-12-11 20:22:48 -0300

    teste cliente with knex.js

--- knexfile.ts
+++ knexfile.ts
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: "sqlite3",
+    client: "pg",
     connection: {
       user: process.env.PGUSER,
       password: process.env.PGPASSWORD,
