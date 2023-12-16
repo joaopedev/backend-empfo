5651966f862f2362d99091329b4498d899182152
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2023-12-16 00:05:32 -0300

    Update knexfile to heroku deploy

--- knexfile.ts
+++ knexfile.ts
@@ -1,39 +1,35 @@
-import type { Knex } from "knex";
 require("dotenv").config();
 
-const config: { [key: string]: Knex.Config } = {
-  production: {
-    client: "postgresql",
-    connection: {
-      connectionString: process.env.PGHOST,
-      uri: process.env.PGURI,
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {      
       host: process.env.PGHOST,
       user: process.env.PGUSER,
       password: process.env.PGPASSWORD,
       database: process.env.PGDATABASE,
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
     migrations: {
+      extension: "ts",
       tableName: "knex_migrations",
       directory: "./migrations",
     },
   },
-  test: {
+  production: {
     client: "pg",
-    connection: {
-      connectionString: process.env.PGHOST,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      uri: process.env.PGURI,
-      host: process.env.PGHOST ,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    connection: {      
+      host: process.env.PGHOST,
       user: process.env.PGUSER,
       password: process.env.PGPASSWORD,
       database: process.env.PGDATABASE,
     },
-  }}
-
-module.exports = config;
+    migrations: {
+      extension: "ts",
+      tableName: "knex_migrations",
+      directory: "./migrations",
+    },
+  },
+};
